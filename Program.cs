eed7bb5723a3c1b15d3869ce96a1f67eee95a021
MalamaKangwa <MalamaKangwa@szi.gov.zm>
2020-02-06 14:59:41 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SECS_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
